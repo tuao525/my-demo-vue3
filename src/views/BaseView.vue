cb10115e8807d03f0f8b59bbcfbbee4253a5b1b0
ao.tu <2250467773@qq.com>
2023-11-09 15:59:24 +0800

    feat: 计算属性

--- src/views/BaseView.vue
+++ src/views/BaseView.vue
@@ -3,7 +3,7 @@
  * @email: 2250467773@qq.com
  * @Date: 2023-11-09 10:55:25
  * @LastEditors: Do not edit
- * @LastEditTime: 2023-11-09 11:03:47
+ * @LastEditTime: 2023-11-09 14:46:32
 -->
 <template>
   <div class="home">
@@ -25,7 +25,7 @@ const data = reactive({
   message: '我是vue3响应式基础',
   count: 1
 })
-const countRef = ref(0)
+const countRef = ref(1)
 const object = { foo: ref(0) }
 function click () {
   data.count += 1
--- src/views/ListRender.vue
+++ src/views/ListRender.vue
@@ -3,38 +3,55 @@
  * @email: 2250467773@qq.com
  * @Date: 2023-11-09 10:55:32
  * @LastEditors: Do not edit
- * @LastEditTime: 2023-11-09 11:00:15
+ * @LastEditTime: 2023-11-09 15:56:44
 -->
 <template>
   <div class="home">
-    {{ data.message }}
-    <p>
-      <button @click="click">点击 {{ data.count }} - {{ countRef }}</button>
-    </p>
-    <p>{{ object.foo + 1 }} - {{ object.foo.value + 1 }} - {{ object.foo }}</p>
+    <div v-for='item in itemsRef' :key='item'> {{item.message}} </div>
+    <ul>
+      <li v-for='(item, index) in itemsRef' :key='index'>{{parentMessage}} - {{index}} - {{item.message}}</li>
+    </ul>
+    <h4>解构</h4>
+    <ul>
+      <li v-for='({message}, index) in itemsRef' :key='index'>{{parentMessage}} - {{index}} - {{message}}</li>
+    </ul>
+    <h4>使用作为分隔符代替in</h4>
+    <ul>
+      <li v-for='({message}, index) of itemsRef' :key='index'>{{parentMessage}} - {{index}} - {{message}}</li>
+    </ul>
+    <h4>遍历对象</h4>
+    <ul>
+      <!-- 第一个参数为属性值，第二个参数为属性名，第三个为索引 -->
+      <li v-for='(item,key,index) in myObject' :key='index'>{{ key }}:  {{ item }}</li>
+    </ul>
+    <h4>展示过滤或排序后的结果</h4>
+    <ul>
+      <li v-for='item in evenNumbers' :key='item'>{{item}}</li>
+    </ul>
+    <h4>多层嵌套</h4>
+    <ul v-for='(item,index) in sets' :key='index'>
+      <li v-for='n in even(item)' :key='n'>{{ n }}</li>
+    </ul>
   </div>
 </template>
 
 <script setup>
-import { reactive, ref } from 'vue'
-// @ is an alias to /src
-// export default {
-// name: 'baseView',
-// setup () {
-const data = reactive({
-  message: '我是vue3响应式基础',
-  count: 1
+import { reactive, ref, computed } from 'vue'
+const myObject = reactive({
+  title: 'How to do lists in Vue',
+  author: 'Jane Doe',
+  publishedAt: '2016-04-10'
 })
-const countRef = ref(0)
-const object = { foo: ref(0) }
-function click () {
-  data.count += 1
-  countRef.value++
+const parentMessage = ref('Parent')
+const itemsRef = ref(
+  [{ message: 'bmw' }, { message: 'bba' }])
+const numbers = ref([1, 2, 3, 4, 5])
+const sets = ref([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+
+const evenNumbers = computed(() => {
+  return numbers.value.filter(item => item % 2 === 0)
+})
+const even = (number) => {
+  return number.filter(item => item % 2 === 0)
 }
-// return {
-//   data,
-//   click
-// }
-// }
-// }
 </script>
