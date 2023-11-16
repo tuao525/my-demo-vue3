9838381c506b6660826afc977728a4040b2cafbf
ao.tu <2250467773@qq.com>
2023-11-16 16:38:31 +0800

    feat: 插槽

--- src/views/component/SlotDemo.vue
+++ src/views/component/SlotDemo.vue
@@ -3,7 +3,7 @@
  * @email: 2250467773@qq.com
  * @Date: 2023-11-09 10:55:25
  * @LastEditors: Do not edit
- * @LastEditTime: 2023-11-16 16:30:15
+ * @LastEditTime: 2023-11-16 16:38:23
 -->
 <template>
   <div class="home">
@@ -12,7 +12,6 @@
         <button>
           <slot  :title="1">submit</slot>
         </button>
-
         <slot name="header"></slot>
         <slot name="active" :title="123456"></slot>
     </div>
